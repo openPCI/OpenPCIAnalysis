8eb6390c0599fc1741e3a202690993bb26a8825b
Jeppe Bundsgaard <jeppe@bundsgaard.net>
2020-02-24 11:17:53 +0100

    Allow more than one column to be imported at a time

--- R/room.R
+++ R/room.R
@@ -1,8 +1,8 @@
 #' mergeScores
 #'
 #' @param result result of scoring script. A matrix. 
-#' @param resp Response data.frame from TAO
-#' @param column the column to merge with the results
+#' @param resp Response data.frame from TAO or from pre-coded items.
+#' @param column String. The beginning of the name of the columns to merge with the results. If more columns start with the provided string, all of them are included.
 #' @param prefix Text to put before the variable name
 #'
 #' @return returns a result matrix
@@ -11,22 +11,25 @@
 #' @examples
 #' result<-mergeScores(result,prescoredresults,"X2..Responses.RESPONSE","TM2_")
 mergeScores<-function(result,resp,column,prefix="") {
-  if(column %in% names(resp)) {
-    respcolumn<-resp[,column]
-    respcolumn[respcolumn==""]<-"[]"
-    all<-apply(as.array(respcolumn),1,jsonlite::fromJSON)
-    i<-1
-    while(length(all[[i]])==0) i<-i+1
-    numelm<-length(all[[i]][["score"]])
-    names<-names(all[[i]][["score"]])
-    scores<-as.data.frame(t(sapply(all,function(x) {y<-unlist(x$score);if(is.null(y)) rep(0,numelm) else y})))
-    print(names)
-    names(scores)<-paste0(prefix,names)
-    testTaker<-data.frame(testTaker=resp[,1])
-    scoresAndTaker<-cbind(testTaker,scores)
-    #scoresAndTaker<-scoresAndTaker[apply(scores,1,function(x) {sum(is.na(x))!=length(x)}),]
-    result<-merge(result,scoresAndTaker,by.x="testTaker",by.y="testTaker")
-  }
+  cols<-grepl(paste0("^",column),names(resp),ignore.case = T)
+  if(any(cols)) {
+    for(column in which(cols)) {
+      respcolumn<-resp[,column]
+      respcolumn[respcolumn==""]<-"[]"
+      all<-apply(as.array(respcolumn),1,jsonlite::fromJSON)
+      i<-1
+      while(length(all[[i]])==0) i<-i+1
+      numelm<-length(all[[i]][["score"]])
+      names<-names(all[[i]][["score"]])
+      scores<-as.data.frame(t(sapply(all,function(x) {y<-unlist(x$score);if(is.null(y)) rep(0,numelm) else y})))
+      print(names)
+      names(scores)<-paste0(prefix,names)
+      testTaker<-data.frame(testTaker=resp[,1])
+      scoresAndTaker<-cbind(testTaker,scores)
+      #scoresAndTaker<-scoresAndTaker[apply(scores,1,function(x) {sum(is.na(x))!=length(x)}),]
+      result<-merge(result,scoresAndTaker,by.x=1,by.y=1)
+    }
+  } else warning(paste("Merge Scores: No column",column,"in response data.frame"))
   return (result) 
 }
 
