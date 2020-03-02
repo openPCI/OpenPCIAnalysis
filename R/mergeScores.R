878aeb1259e6171e1b140b0618f9b5403d08d68c
Jeppe Bundsgaard <jeppe@bundsgaard.net>
2020-03-02 19:53:04 +0100

    Allow more than one column to be imported at a time

--- R/mergeScores.R
+++ R/mergeScores.R
@@ -12,7 +12,7 @@
 #' @examples
 #' # Make sure not to import data from item X20
 #' result<-mergeScores(result,prescoredresults,"X2[^0-9]","TM2_")
-mergeScores<-function(result,resp,column,prefix="",test.taker=NULL) {
+mergeScores<-function(result,resp,column,test.taker=NULL,prefix="") {
   if(!is.null(test.taker)) resp<-cbind(data.frame(test.taker=test.taker),resp)
   cols<-grepl(paste0("^",column),names(resp),ignore.case = T)
   if(any(cols)) {
